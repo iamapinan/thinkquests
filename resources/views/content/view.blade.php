c249af484d03dceaa5acd938d5459312f62d2be3
Apinan Woratrakun <iamapinan@gmail.com>
2025-01-20 15:40:19 +0700

    Update Docker image and enhance quiz functionality
    - Changed Docker image from 'elearning' to 'docker.io/library/thinkquest'.
    - Added conditional rendering for video content in the quiz view.
    - Improved JavaScript functions for better organization and readability, including sound effects and score saving functionality.
    - Enhanced user experience by adding checks for content availability and updating the quiz result display.

--- resources/views/content/view.blade.php
+++ resources/views/content/view.blade.php
@@ -18,7 +18,9 @@
                 @if(isset($content->plan))
                 <li><a href="{{ route('content.tab', ['id' => $content->id, 'tab' => 'plan']) }}" class="block rounded py-2 px-4 {{ isset($tab) && $tab == 'plan' ? 'bg-blue-300' : '' }}">แผนการสอน</a></li>
                 @endif
+                @if(isset($content->video_pdf))
                 <li><a href="{{ route('content.tab', ['id' => $content->id, 'tab' => 'videos']) }}" class="block rounded py-2 px-4 {{ isset($tab) && $tab == 'videos' ? 'bg-blue-300' : '' }}">เนื้อหา</a></li>
+                @endif
                 <li><a href="{{ route('content.tab', ['id' => $content->id, 'tab' => 'e-testing']) }}" class="block rounded py-2 px-4 {{ isset($tab) && $tab == 'e-testing' ? 'bg-blue-300' : '' }}">แบบทดสอบ</a></li>
             </ul>
         </div>
@@ -61,7 +63,7 @@
                             <iframe src="{{ asset('storage/'.$content->video_pdf) }}" width="100%" style="height: 80vh;"></iframe>
                         @else
                             <video controls width="100%" style="height: 80vh;">
-                                <source src="{{ asset('storage/'.$content->video) }}" type="video/mp4">
+                                <source src="{{ asset('storage/'.$content->video_pdf) }}" type="video/mp4">
                                 Your browser does not support the video tag.
                             </video>
                         @endif
@@ -98,202 +100,202 @@
         // },
     ];
 
-const questionNumber = document.querySelector(".question-number");
-const questionText = document.querySelector(".question-text");
-const optionContainer = document.querySelector(".option-container");
-const answersIndicatorContainer = document.querySelector(".answers-indicator");
-const homeBox = document.querySelector(".home-box");
-const quizBox = document.querySelector(".quiz-box");
-const resultBox = document.querySelector(".result-box");
-const questionLimit = quiz.length;
-const allScore = quiz.reduce((sum, current) => sum + current.score, 0);
-const questionTime = 30;
-let currentScore = 0;
+    const questionNumber = document.querySelector(".question-number");
+    const questionText = document.querySelector(".question-text");
+    const optionContainer = document.querySelector(".option-container");
+    const answersIndicatorContainer = document.querySelector(".answers-indicator");
+    const homeBox = document.querySelector(".home-box");
+    const quizBox = document.querySelector(".quiz-box");
+    const resultBox = document.querySelector(".result-box");
+    const questionLimit = quiz.length;
+    const allScore = quiz.reduce((sum, current) => sum + current.score, 0);
+    const questionTime = 30;
+    let currentScore = 0;
 
 
-let questionCounter = 0;
-let currentQuestion;
-let availableQuestions = [];
-let availableOptions = [];
-let correctAnswers = 0;
-let attempt = 0;
+    let questionCounter = 0;
+    let currentQuestion;
+    let availableQuestions = [];
+    let availableOptions = [];
+    let correctAnswers = 0;
+    let attempt = 0;
 
-function setAvailableQuestions(){
-    const totalQuestions = quiz.length;
-    for(let i=0; i<totalQuestions; i++){
-        availableQuestions.push(quiz[i])
+    function setAvailableQuestions(){
+        const totalQuestions = quiz.length;
+        for(let i=0; i<totalQuestions; i++){
+            availableQuestions.push(quiz[i])
+        }
     }
-}
 
-function getNewQuestion(){
-    const questionIndex = availableQuestions[Math.floor(Math.random()*availableQuestions.length)];
-    currentQuestion = questionIndex;
-    questionNumber.innerHTML = "<span class='text-xl'>คำถามที่ " + (questionCounter+1) + " จาก " + questionLimit + "</span> <span class='text-green-500'>⭐️ " + currentQuestion.score + " คะแนน</span>";
-    questionText.innerHTML = currentQuestion.q;
-    const index1 = availableQuestions.indexOf(questionIndex);
-    availableQuestions.splice(index1,1);
-    if(currentQuestion.hasOwnProperty("img")){
-        const img = document.createElement("img");
-        img.src = currentQuestion.img;
-        questionText.appendChild(img);
+    function getNewQuestion(){
+        const questionIndex = availableQuestions[Math.floor(Math.random()*availableQuestions.length)];
+        currentQuestion = questionIndex;
+        questionNumber.innerHTML = "<span class='text-xl'>คำถามที่ " + (questionCounter+1) + " จาก " + questionLimit + "</span> <span class='text-green-500'>⭐️ " + currentQuestion.score + " คะแนน</span>";
+        questionText.innerHTML = currentQuestion.q;
+        const index1 = availableQuestions.indexOf(questionIndex);
+        availableQuestions.splice(index1,1);
+        if(currentQuestion.hasOwnProperty("img")){
+            const img = document.createElement("img");
+            img.src = currentQuestion.img;
+            questionText.appendChild(img);
+        }
+        const optionsLength = currentQuestion.options.length;
+        for(let i=0; i<optionsLength; i++){
+            availableOptions.push(i);
+        }
+        optionContainer.innerHTML = '';
+        let animationDelay = 0.2;
+        for(let i=0; i<optionsLength; i++){
+            const optionIndex = availableOptions[Math.floor(Math.random()*availableOptions.length)];
+            const index2 = availableOptions.indexOf(optionIndex);
+            availableOptions.splice(index2,1);
+            const option = document.createElement("div");
+            option.innerHTML = currentQuestion.options[optionIndex];
+            option.id = optionIndex;
+            option.style.animationDelay = animationDelay+'s';
+            animationDelay = animationDelay+0.15;
+            option.className = "option";
+            optionContainer.appendChild(option);
+            option.setAttribute("onclick", "getResult(this)");
+        }
+        questionCounter++;
     }
-    const optionsLength = currentQuestion.options.length;
-    for(let i=0; i<optionsLength; i++){
-        availableOptions.push(i);
-    }
-    optionContainer.innerHTML = '';
-    let animationDelay = 0.2;
-    for(let i=0; i<optionsLength; i++){
-        const optionIndex = availableOptions[Math.floor(Math.random()*availableOptions.length)];
-        const index2 = availableOptions.indexOf(optionIndex);
-        availableOptions.splice(index2,1);
-        const option = document.createElement("div");
-        option.innerHTML = currentQuestion.options[optionIndex];
-        option.id = optionIndex;
-        option.style.animationDelay = animationDelay+'s';
-        animationDelay = animationDelay+0.15;
-        option.className = "option";
-        optionContainer.appendChild(option);
-        option.setAttribute("onclick", "getResult(this)");
-    }
-    questionCounter++;
-}
 
-function getResult(element){
-    const id = parseInt(element.id);
-    if(id === currentQuestion.answer){
-        element.classList.add("correct");
-        updateAnswerIndicator("correct");
-        correctAnswers++;
-        playSound("correct");
-        currentScore += currentQuestion.score;
+    function getResult(element){
+        const id = parseInt(element.id);
+        if(id === currentQuestion.answer){
+            element.classList.add("correct");
+            updateAnswerIndicator("correct");
+            correctAnswers++;
+            playSound("correct");
+            currentScore += currentQuestion.score;
+        }
+        else{
+            element.classList.add("wrong");
+            updateAnswerIndicator("wrong");
+            const optionLength = optionContainer.children.length;
+            for(let i=0; i<optionLength; i++){
+                if(parseInt(optionContainer.children[i].id)===currentQuestion.answer) {
+                    optionContainer.children[i].classList.add("correct");
+                }
+            }
+            playSound("wrong");
+
+        }
+        attempt++;
+        unclickableOptions();
     }
-    else{
-        element.classList.add("wrong");
-        updateAnswerIndicator("wrong");
+
+    function playSound(soundType) {
+        // Add code here to play the sound based on the soundType
+        // soundType is either 'correct' or 'wrong'
+        const audio = new Audio(`/resources/sounds/${soundType}.mp3`);
+        audio.play();
+    }
+
+    function unclickableOptions(){
         const optionLength = optionContainer.children.length;
         for(let i=0; i<optionLength; i++){
-            if(parseInt(optionContainer.children[i].id)===currentQuestion.answer) {
-                optionContainer.children[i].classList.add("correct");
+            optionContainer.children[i].classList.add("already-answered");
+        }
+    }
+
+    function next(){
+        if(questionCounter === questionLimit){
+            quizOver();
+        }else{
+            getNewQuestion()
+        }
+    }
+
+    function answersIndicator(){
+        answersIndicatorContainer.innerHTML = '';
+        const totalQuestion = questionLimit;
+        for(let i=0; i<totalQuestion; i++){
+            const indicator = document.createElement("div");
+            answersIndicatorContainer.appendChild(indicator);
+
+        }
+    }
+
+    function updateAnswerIndicator(markType){
+        answersIndicatorContainer.children[questionCounter-1].classList.add(markType)
+    }
+
+    function quizOver(){
+        quizBox.classList.add("hide");
+        resultBox.classList.remove("hide");
+        quizResult();
+    }
+
+    function quizResult(){
+        resultBox.querySelector(".total-question").innerHTML = questionLimit;
+        resultBox.querySelector(".total-correct").innerHTML = correctAnswers;
+        resultBox.querySelector(".total-attempt").innerHTML = attempt;
+        resultBox.querySelector(".total-wrong").innerHTML = attempt - correctAnswers;
+        const percentage = (correctAnswers/questionLimit)*100;
+        resultBox.querySelector(".percentage").innerHTML = percentage.toFixed(2) + "%";
+        resultBox.querySelector(".total-score").innerHTML = currentScore + " / " + allScore;
+    }
+
+    function resetQuiz(){
+        questionCounter = 0;
+        correctAnswers = 0;
+        attempt = 0;
+        availableQuestions = [];
+    }
+
+    function tryAgain(){
+        resultBox.classList.add("hide");
+        quizBox.classList.remove("hide");
+        resetQuiz();
+        startQuiz();
+    }
+
+    function goToHome(){
+        resultBox.classList.add("hide");
+        homeBox.classList.remove("hide");
+        saveScoreToDatabase();
+        resetQuiz();
+    }
+
+    function startQuiz(){
+        homeBox.classList.add("hide");
+        quizBox.classList.remove("hide");
+        setAvailableQuestions();
+        getNewQuestion();
+        answersIndicator();
+    }
+
+    window.onload = function (){
+        homeBox.querySelector(".total-question").innerHTML = ""+questionLimit;
+        homeBox.querySelector(".total-score").innerHTML = ""+allScore;
+    }
+
+    async function saveScoreToDatabase() {
+        const scoreData = {
+            score: currentScore,
+            content_id: {{ $content->id }}
+        };
+
+        try {
+            const response = await fetch("{{ route('user-scores.save', Auth::user()->id) }}", {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content'),
+                },
+                body: JSON.stringify(scoreData),
+            });
+            if(response.ok) {
+                const data = await response.json(); // Parse the response as JSON
+                alert(data.message);
             }
+
+            console.log('Response:', response);
+            
+        } catch (error) {
+            console.error('Error:', error);
         }
-        playSound("wrong");
-
     }
-    attempt++;
-    unclickableOptions();
-}
-
-function playSound(soundType) {
-    // Add code here to play the sound based on the soundType
-    // soundType is either 'correct' or 'wrong'
-    const audio = new Audio(`/resources/sounds/${soundType}.mp3`);
-    audio.play();
-}
-
-function unclickableOptions(){
-    const optionLength = optionContainer.children.length;
-    for(let i=0; i<optionLength; i++){
-        optionContainer.children[i].classList.add("already-answered");
-    }
-}
-
-function next(){
-    if(questionCounter === questionLimit){
-        quizOver();
-    }else{
-        getNewQuestion()
-    }
-}
-
-function answersIndicator(){
-    answersIndicatorContainer.innerHTML = '';
-    const totalQuestion = questionLimit;
-    for(let i=0; i<totalQuestion; i++){
-        const indicator = document.createElement("div");
-        answersIndicatorContainer.appendChild(indicator);
-
-    }
-}
-
-function updateAnswerIndicator(markType){
-    answersIndicatorContainer.children[questionCounter-1].classList.add(markType)
-}
-
-function quizOver(){
-    quizBox.classList.add("hide");
-    resultBox.classList.remove("hide");
-    quizResult();
-}
-
-function quizResult(){
-    resultBox.querySelector(".total-question").innerHTML = questionLimit;
-    resultBox.querySelector(".total-correct").innerHTML = correctAnswers;
-    resultBox.querySelector(".total-attempt").innerHTML = attempt;
-    resultBox.querySelector(".total-wrong").innerHTML = attempt - correctAnswers;
-    const percentage = (correctAnswers/questionLimit)*100;
-    resultBox.querySelector(".percentage").innerHTML = percentage.toFixed(2) + "%";
-    resultBox.querySelector(".total-score").innerHTML = currentScore + " / " + allScore;
-}
-
-function resetQuiz(){
-    questionCounter = 0;
-    correctAnswers = 0;
-    attempt = 0;
-    availableQuestions = [];
-}
-
-function tryAgain(){
-    resultBox.classList.add("hide");
-    quizBox.classList.remove("hide");
-    resetQuiz();
-    startQuiz();
-}
-
-function goToHome(){
-    resultBox.classList.add("hide");
-    homeBox.classList.remove("hide");
-    saveScoreToDatabase();
-    resetQuiz();
-}
-
-function startQuiz(){
-    homeBox.classList.add("hide");
-    quizBox.classList.remove("hide");
-    setAvailableQuestions();
-    getNewQuestion();
-    answersIndicator();
-}
-
-window.onload = function (){
-    homeBox.querySelector(".total-question").innerHTML = ""+questionLimit;
-    homeBox.querySelector(".total-score").innerHTML = ""+allScore;
-}
-
-async function saveScoreToDatabase() {
-    const scoreData = {
-        score: currentScore,
-        content_id: {{ $content->id }}
-    };
-
-    try {
-        const response = await fetch("{{ route('user-scores.save', Auth::user()->id) }}", {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content'),
-            },
-            body: JSON.stringify(scoreData),
-        });
-        if(response.ok) {
-            const data = await response.json(); // Parse the response as JSON
-            alert(data.message);
-        }
-
-        console.log('Response:', response);
-        
-    } catch (error) {
-        console.error('Error:', error);
-    }
-}
 </script>
